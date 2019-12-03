9c5338ed04c34e0cb04c0bcaba73c8d97f0a7898
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-03 23:45:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,15 @@ def base_hash
 end 
 
 def monopoly_with_second_tier
-  {:railroads => {:pieces => "4"}
+  {:railroads => {:pieces => 4}
   }
 end
 
 def monopoly_with_third_tier
- {:railroads => {:pieces => "4", :rent_in_dollars => [], [], [], []} 
+ {
+  :railroads => {
+  :pieces => 4, 
+  :rent_in_dollars => [], [], [], []}
   }
 end
 
